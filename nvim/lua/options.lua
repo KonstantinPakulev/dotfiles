b28d4b8c86c01d3ac59226dad275cd9bae81122b
KonstantinPakulev <ankalagon_black@mail.ru>
2026-03-30 13:17:54 +0300

    Clipboard fix

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,6 +2,19 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 vim.o.autoread = true
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
   command = "checktime",
